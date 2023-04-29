db29baf43e0c740a11c09be75f0619831e19cef8
Javier Guerrero <javier.guerrero.c2001@gmail.com>
2023-04-29 13:37:12 -0600

    login y registro funcionan
    funcionan correctamente utilizando local storage

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { SweetAlertArrayOptions } from 'sweetalert2';
+import Swal from 'sweetalert2';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -19,12 +21,48 @@ export class LoginComponent {
   }
 
   onSubmit(): void{
+    // Se obtiene el objeto usuarios desde el almacenamiento local (si no existe, se inicializa como un objeto vacío)
+    var usuarios = JSON.parse(localStorage.getItem("usuarios") || '{}');
+    // Se obtiene el nombre de usuario del formulario y se convierte a una cadena
+    var alias = JSON.stringify(this.loginForm.value.usuario);
+    // Se guarda el nombre de usuario actual en el almacenamiento local
+    localStorage.setItem("actual", alias);
 
-
-    localStorage.setItem('usr', JSON.stringify(this.loginForm.value.usuario));
-    localStorage.setItem('pass', JSON.stringify(this.loginForm.value.contrasenia));
-
+    // Se obtiene el objeto de usuarios desde el almacenamiento local y se convierte en un objeto de JavaScript
+    var user: any = localStorage.getItem("usuarios");
+    user = JSON.parse(user);
+    // Se establece la variable "existe" en 0 (es decir, no existe el usuario)
+    var existe = 0;
+    // Se recorre el objeto de usuarios para comprobar si el usuario ya existe
+    for (var i in user) {
+      if (i == localStorage.getItem("actual")) {
+          // Si el usuario ya existe, se establece la variable "existe" en 1 y se sale del bucle
+          existe=1;
+          const storedPassword = usuarios[alias].pass;
+          const enteredPassword = this.loginForm.value.contrasenia;
+          console.log(storedPassword + "-" + enteredPassword);
+          if(storedPassword === enteredPassword){
+            console.log("login correcto");
+            Swal.fire(
+              'Exito!',
+              'Has iniciado sesion!',
+              'success'
+            )
+          }else{
+            console.log("login incorrecto");
+            Swal.fire(
+              'Error!',
+              'contraseña incorrecto',
+              'error'
+            )
+          }
+          // Se podría mostrar una alerta aquí
+          break;
+      }
+    }
+    
   }
+  
 
 
 }
