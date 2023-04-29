71e2ef7cb7ea3f94fed535e2d475e9bb1535059a
Javier Guerrero <javier.guerrero.c2001@gmail.com>
2023-04-29 00:05:59 -0600

    login se guarda en local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,6 +32,8 @@ import { RegistrarComponent } from './components/registrar/registrar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  loginForm = this.formBuilder.group({
+    usuario: '',
+    contrasenia: ''
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  onSubmit(): void{
+
+
+    localStorage.setItem('usr', JSON.stringify(this.loginForm.value.usuario));
+    localStorage.setItem('pass', JSON.stringify(this.loginForm.value.contrasenia));
+
+  }
+
 
 }
